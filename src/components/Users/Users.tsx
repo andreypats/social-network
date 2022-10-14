f6065b5f8641ecd4234794d8650fdab3aa648def
andreypats <andrey.it.work@gmail.com>
2022-10-14 20:46:30 +0300

    Lesson 53 (extends React.Component) completed

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -4,37 +4,33 @@ import {UserType} from "../../redux/users-reducer";
 import axios from "axios";
 import userPhoto from '../../assets/images/img.png'
 
-export const Users = (props: any) => {
-    let getUsers = () => {
-        if (props.users.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users')
-                .then(response => {
-                    props.setUsers(response.data.items)
-                });
-        }
+export class Users extends React.Component< any, any >{
+
+    constructor(props: any) {
+        super(props);
+
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            });
     }
 
-    // {id: 1, photoUrl: 'https://cdn.images.express.co.uk/img/dynamic/79/590x/arnie-body-builder-series-407806.jpg', followed: true, fullName: 'Andrey', status: 'I am champ', location: {city: 'Orsha', country: 'Belarus'}},
-    // {id: 2, photoUrl: 'https://cdn.images.express.co.uk/img/dynamic/79/590x/arnie-body-builder-series-407806.jpg', followed: true, fullName: 'Maxim', status: 'I am businessman', location: {city: 'Mogilev', country: 'Belarus'}},
-    // {id: 3, photoUrl: 'https://cdn.images.express.co.uk/img/dynamic/79/590x/arnie-body-builder-series-407806.jpg', followed: false, fullName: 'Stas', status: 'I am dreamer', location: {city: 'St.Petersburg', country: 'Russia'}},
-    // {id: 4, photoUrl: 'https://cdn.images.express.co.uk/img/dynamic/79/590x/arnie-body-builder-series-407806.jpg', followed: false, fullName: 'Artem', status: 'I am rider', location: {city: 'Kazan', country: 'Russia'}},
-
-    return (
-        <div>
-            <button onClick={getUsers}>Get Users</button>
-            {
-                props.users.map((u: UserType) => <div key={u.id}>
+    render() {
+        return (
+            <div>
+                {
+                    this.props.users.map((u: UserType) => <div key={u.id}>
                     <span>
                         <div>
                             <img src={u.photos.small != null ? u.photos.small : userPhoto} className={s.userPhoto} alt={'User photo'}/>
                         </div>
                         <div>
                             {u.followed
-                                ? <button onClick={()=>{props.unfollow(u.id)}}>Unfollow</button>
-                                : <button onClick={()=>{props.follow(u.id)}}>Follow</button>}
+                                ? <button onClick={()=>{this.props.unfollow(u.id)}}>Unfollow</button>
+                                : <button onClick={()=>{this.props.follow(u.id)}}>Follow</button>}
                         </div>
                     </span>
-                    <span>
+                        <span>
                         <span>
                             <div>{u.name}</div>
                             <div>{u.status}</div>
@@ -44,8 +40,16 @@ export const Users = (props: any) => {
                             <div>{'u.location.city'}</div>
                         </span>
                     </span>
-                </div>)
-            }
-        </div>
-    );
-};
+                    </div>)
+                }
+            </div>
+        );
+    }
+}
+
+
+
+// {id: 1, photoUrl: 'https://cdn.images.express.co.uk/img/dynamic/79/590x/arnie-body-builder-series-407806.jpg', followed: true, fullName: 'Andrey', status: 'I am champ', location: {city: 'Orsha', country: 'Belarus'}},
+// {id: 2, photoUrl: 'https://cdn.images.express.co.uk/img/dynamic/79/590x/arnie-body-builder-series-407806.jpg', followed: true, fullName: 'Maxim', status: 'I am businessman', location: {city: 'Mogilev', country: 'Belarus'}},
+// {id: 3, photoUrl: 'https://cdn.images.express.co.uk/img/dynamic/79/590x/arnie-body-builder-series-407806.jpg', followed: false, fullName: 'Stas', status: 'I am dreamer', location: {city: 'St.Petersburg', country: 'Russia'}},
+// {id: 4, photoUrl: 'https://cdn.images.express.co.uk/img/dynamic/79/590x/arnie-body-builder-series-407806.jpg', followed: false, fullName: 'Artem', status: 'I am rider', location: {city: 'Kazan', country: 'Russia'}},
