c5da2a8d1ede8ffb01c90ccf096a39fb8c0ee72b
andreypats <andrey.it.work@gmail.com>
2022-11-11 00:58:15 +0300

    Lesson 75 (redux form) is learning

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
 
-export const Login = () => {
+const Login = () => {
     return (
-        <h1>
-            Login
-        </h1>
+        <div>
+            <h1>Login</h1>
+            <LoginForm />
+        </div>
+    );
+};
+
+const LoginForm = () => {
+    return (
+        <form action="">
+            <div>
+                <input type="text" placeholder={'Login'}/>
+            </div>
+            <div>
+                <input type="text" placeholder={'Password'}/>
+            </div>
+            <div>
+                <input type="checkbox"/> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
     );
 };
 
